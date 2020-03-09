481475562faa6a7dcf31431d7baecb53326cdf2c
ayertdatta <43091745+ayertdatta@users.noreply.github.com>
2020-03-10 03:35:45 +0530

    Add files via upload
    version 1.0

--- application/models/Crud_model.php
+++ application/models/Crud_model.php
@@ -17,12 +17,31 @@ class Crud_model extends CI_Model {
                 }
         }
         function save_data($data,$n){
-                $this->db->insert($n,$data);
+                $this->db->insert($n,$data,TRUE);
         }
         
         function list_table(){
                 $tables = $this->db->list_tables();
                 return $tables;
         }
-	
+	function search_table($n){
+                $query = $this->db->get_where('scheme_table', array('s_name' => $n));
+                $row = $query->row();
+                if (isset($row)){
+                        return $row->name;
+                }
+                else{
+                        return $n;
+                }
+        }
+        function search_attri($n){
+                $query = $this->db->get_where('attri_table', array('a_name' => $n));
+                $row = $query->row();
+                if (isset($row)){
+                        return $row->name;
+                }
+                else{
+                        return $n;
+                }
+        }
 }
