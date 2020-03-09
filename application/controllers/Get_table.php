9c5b0e4bbc0639209cacb8f1a7697eea30dbd3cd
ayertdatta <43091745+ayertdatta@users.noreply.github.com>
2020-03-10 03:34:24 +0530

    Add files via upload
    version 1.0

--- application/controllers/Get_table.php
+++ application/controllers/Get_table.php
@@ -7,6 +7,10 @@ class Get_table extends CI_Controller {
 	{
         $this->load->model('Crud_model');
         $result1['data']=$this->Crud_model->list_table();
+        $result1['s_name']=array();
+        foreach($result1['data'] as $row){
+            $result1['s_name'][]=$this->Crud_model->search_table($row);
+        }
         $this->load->view('ori',$result1);
         if(isset($_POST['sub1'])){
             $requestpage=$_POST['selectPage'];
@@ -22,21 +26,24 @@ class Get_table extends CI_Controller {
 
        //Fetch Attribute name. n - schema name
        $result['data'] =$this->Crud_model->get_table($n);
-       $result['name'] =$n;
+       $result['name'] =$this->Crud_model->search_table($n);
         
        #dynamic type checking
        $x=$this->Crud_model->get_attri($n);
        $y=array();
+       $result['s_name']=array();
        foreach($x as $field){
            if($field->name=="id"){
-               continue;
+                $result['s_name'][]=$this->Crud_model->search_attri($field->name);
+                continue;
            }
            $z=$field->max_length;
            $w=$this->Crud_model->get_type($field->type);
+           $result['s_name'][]=$this->Crud_model->search_attri($field->name);
            
            $y[]=array(
             'field' => $field->name,
-            'label' => $field->name,
+            'label' => $this->Crud_model->search_attri($field->name),
             'rules' => "required|max_length[$z]|$w"
            );
        }
