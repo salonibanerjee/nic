43a2bdda6a8eea1b68a5c99d6f41425d6ad8a3a6
Sayak <sayakdas1999@gmail.com>
2020-03-28 18:06:44 +0530

    Pull

--- application/views/get_table.php
+++ application/views/get_table.php
@@ -26,7 +26,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="../../index3.html" class="nav-link">Home</a>
+        <a href="<?php echo base_url();?>Get_table/ind" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
