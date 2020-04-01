4942503ac8933457e4dc9bb9be0088f27d6bca7f
ayertdatta <ganguly.riddhinath@gmail.com>
2020-04-01 22:52:14 +0530

    minute store_cache changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -6,33 +6,6 @@ class Admin_model extends CI_Model {
         $query = $this->db->get_where('Login',array('username'=>$uname,'active_status'=>1));
         $row = $query->row();
         if($row){
-            //from user_type table
-            $query_user_type = $this->db->get_where('user_type',array('user_type_id_pk'=>$row->user_type_id_fk));
-            $row1 = $query_user_type->row();
-
-            //from user_privilege table get multiple tuples referring from user_type table
-            $query_user_privilege = $this->db->get_where('user_privilege',array('user_type_id_fk'=>$row1->user_type_id_pk));
-            $table2 = $query_user_privilege->result();
-
-            //joining both Privilege and user_privilege tuples
-            $a = array();
-            foreach($table2 as $row_out){
-                $query_privilege = $this->db->get_where('Privilege',array('privilege_id_pk'=>$row_out->privilege_id_fk));
-                $row3 = $query_privilege->row();
-                if($row3->active_status==1){
-                    $a[]=array(
-                            'privilege_id_fk'=> $row_out->privilege_id_fk,
-                            'user_type_id_fk'=> $row_out->user_type_id_fk,
-                            'active_status'=> $row_out->active_status,
-
-                            'parent'=> $row3->parent,
-                            'link'=> $row3->link,
-                            'view_sidebar'=> $row3->view_sidebar,
-                            'order'=> $row3->order
-                        );
-                }
-            }
-
             //user_privilege stores array for multiple tuples
             $result = array(
                 'gp_id' => $row->gp_id,
@@ -41,9 +14,7 @@ class Admin_model extends CI_Model {
                 'designation'=> $row->designation,
                 'user_type_id_fk'=> $row->user_type_id_fk,
                 'id'=> $row->id,
-                'active_status'=> $row->active_status,
-                'desig'=>$row1->desig,
-                'user_privilege'=>$a
+                'active_status'=> $row->active_status
             );
         }else{
             //to remove the null object error when access denied
