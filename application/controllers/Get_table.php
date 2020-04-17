4fdb5f9e352a79905792b5b124365c0728c64b62
Sayak <sayakdas1999@gmail.com>
2020-04-17 12:30:37 +0530

    101

--- application/controllers/Get_table.php
+++ application/controllers/Get_table.php
@@ -8,8 +8,6 @@ class Get_table extends MY_Controller {
 
     public function load($n){
         $this->check_privilege(3);
-
-
         //Load 'CRUD' model
         $this->load->model('profile_model');
         $this->load->model('Crud_model');
@@ -19,7 +17,6 @@ class Get_table extends MY_Controller {
                                             current_url(),
                                             'Backup & draft create- '.$n,
                                             'Custom Message here');
-
         $da = $this->profile_model->get_profile_info($this->session->userdata('uid'));
         $query = $this->db->get_where('meeting_schedule', array('meeting_id_pk' => 1));
         $row = $query->row();
@@ -38,21 +35,24 @@ class Get_table extends MY_Controller {
                     </script>
             <?php
         }else{
-
             //Fetch Attribute name. n - schema name
+            $result['n']=$n;
             $result['data'] =$this->Crud_model->get_table($n);
             $result['name'] =$this->Crud_model->search_table($n);
-            $result['data_table']=$this->Crud_model->list_table();
-            $result['s_name_table']=$this->Crud_model->fullname();
             $result['username'] = $this->session->userdata('uid');
-            $result['draft_data'] = $this->Crud_model->draft_data_fetch($n."_draft");
+            $mon=$this->input->post('modmonth');
+            $yr = $this->input->post('modyear');
+            if(isset($mon) && isset($yr)){
+                $result['draft_data'] = $this->Crud_model->draft_filter($n."_draft",$mon,$yr);
+            }else{
+                $result['draft_data'] = $this->Crud_model->draft_data_fetch($n."_draft");
+            }
             $result['profile'] = $da;
             $result['month']=array("NULL","January","February","March","April","May","June","July","August","Semptember","October","November","December");
-            if($da['flag']==0){
-                $da['flag']=1;
-                $da['data_table']=$result['data_table'];
-                $da['s_name_table']=$result['s_name_table'];
-            }
+            //if($da['flag']==0){
+            $da['flag']=1;
+            $da['data_table']=$this->Crud_model->list_table();
+            $da['s_name_table']=$this->Crud_model->fullname();
             $this->load->view('dashboard/sidebar',$da);
             #dynamic type checking
             $x=$this->Crud_model->get_attri($n);
@@ -60,72 +60,82 @@ class Get_table extends MY_Controller {
             $result['s_name']=array();
             $result['region']=$this->Crud_model->region_name($this->session->userdata('schcd'));
             $flag=0;
-
             //setting array for form validations
             foreach($x as $field){
-                if($field->name=="id_pk" || $field->name=="login_id_fk" || $field->name=="inserted_at" || $field->name=="ip" || $field->name=='nodal_check'){
-                    $result['s_name'][]=$this->Crud_model->search_attri($field->name);
+                $result['s_name'][]=$this->Crud_model->search_attri($field->name);
+            }
+            $this->load->view('get_table',$result);
+        }
+    }
+    function submit_draft($n){
+        $this->load->model('Crud_model');
+        $mon=$this->input->post('modmonth');
+        $yr = $this->input->post('modyear');
+        if(isset($mon) && isset($yr)){
+            return $this->Crud_model->draft_filter($n."_draft",$mon,$yr);
+        }else{
+            return $this->Crud_model->draft_data_fetch($n."_draft");
+        }
+    }
+    function submit($n){
+        $this->load->driver('cache',array('adapter' => 'file'));
+        $this->load->model('Crud_model');
+        $result['data'] =$this->Crud_model->get_table($n);
+        $x=$this->Crud_model->get_attri($n);
+        $y=array();
+        foreach($x as $field){
+            if($field->name=="id_pk" || $field->name=="login_id_fk" || $field->name=="inserted_at" || $field->name=="ip" || $field->name=='nodal_check'){
+                continue;
+            }
+            $z=$field->max_length;
+            $w=$this->Crud_model->get_type($field->type);
+            $ij=$this->Crud_model->search_attri($field->name);
+            $y[]=array(
+                'field' => $field->name,
+                'label' => $ij,
+                'rules' => "required|max_length[$z]|$w"
+            );
+        }
+        $this->form_validation->set_rules($y);
+        if($this->form_validation->run()==TRUE){
+            $r=array();
+            foreach($result['data'] as $row){
+                if($row=="id_pk" || $row=="schcd"){
                     continue;
                 }
-                $z=$field->max_length;
-                $w=$this->Crud_model->get_type($field->type);
-                $ij=$this->Crud_model->search_attri($field->name);
-                $result['s_name'][]=$ij;
-
-                $y[]=array(
-                    'field' => $field->name,
-                    'label' => $ij,
-                    'rules' => "required|max_length[$z]|$w"
-                );
+                $r[$row]=$this->input->post($row);
             }
-
-            $this->form_validation->set_rules($y);
-
-            if($this->form_validation->run()==TRUE){
-                #Submitting form while saving data
-                //if($this->input->post('save')){
-                    $r=array();
-                    foreach($result['data'] as $row){
-                        if($row=="id_pk" || $row=="schcd"){
-                            continue;
-                        }
-                        $r[$row]=$this->input->post($row);
-                    }
-                    $r['login_id_fk'] = $this->cache->get('Active_status')['Login_id_pk'];
-                    $r['inserted_at'] = date('Y-m-d H:i:s');
-                    $r['ip'] = $this->input->ip_address();
-                    $r['schcd'] = $this->session->userdata('schcd');
-                    $r['nodal_check'] = -1;
-                    $old_value=$this->Crud_model->unique_data_entry($n.'_draft',$r['session'],$r['month']);
-                    if($old_value){
-                        $this->Crud_model->update($r,$n.'_draft');
-                        unset($old_value->id_pk);
-                        $this->Crud_model->save_data($old_value,$n.'_backup');
-                        $this->Crud_model->audit_upload($this->session->userdata('uid'),
-                                                    current_url(),
-                                                    'Update - into '.$n.'_draft',
-                                                    'Custom Message here');
-                        //echo "Updated Successfully";
-                    }else{
-                        $this->Crud_model->save_data($r,$n.'_draft');
-                        $this->Crud_model->audit_upload($this->session->userdata('uid'),
-                                                    current_url(),
-                                                    'Insert - into '.$n.'_draft',
-                                                    'Custom Message here');
-                        //echo "Records Saved Successfully";
-                    }
-
-                    //commit and rollback
-                    if($this->db->trans_status()==FALSE)
-                        $this->db->trans_rollback();
-                    else{
-                        $this->db->trans_commit();
-                    }
-                //}
+            $r['login_id_fk'] = $this->cache->get('Active_status')['Login_id_pk'];
+            $r['inserted_at'] = date('Y-m-d H:i:s');
+            $r['ip'] = $this->input->ip_address();
+            $r['schcd'] = $this->session->userdata('schcd');
+            $r['nodal_check'] = -1;
+            $old_value=$this->Crud_model->unique_data_entry($n.'_draft',$r['session'],$r['month']);
+            if($old_value){
+                $this->Crud_model->update($r,$n.'_draft');
+                unset($old_value->id_pk);
+                $this->Crud_model->save_data($old_value,$n.'_backup');
+                $this->Crud_model->audit_upload($this->session->userdata('uid'),
+                                            current_url(),
+                                            'Update - into '.$n.'_draft',
+                                            'Custom Message here');
+                echo "Records Updated Successfully";
             }else{
-                $this->load->view('get_table',$result);
-                echo validation_errors();
+                $this->Crud_model->save_data($r,$n.'_draft');
+                $this->Crud_model->audit_upload($this->session->userdata('uid'),
+                                            current_url(),
+                                            'Insert - into '.$n.'_draft',
+                                            'Custom Message here');
+                echo "Records Saved Successfully";
             }
+            //commit and rollback
+            if($this->db->trans_status()==FALSE)
+                $this->db->trans_rollback();
+            else{
+                $this->db->trans_commit();
+            }
+        }else{
+            echo validation_errors();
         }
     }
 }
