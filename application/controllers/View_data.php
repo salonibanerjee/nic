536b1ef0f60cf4b52b110182f6a38b43d5c434f0
Sayak <sayakdas1999@gmail.com>
2020-04-22 19:38:06 +0530

    F

--- application/controllers/View_data.php
+++ application/controllers/View_data.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Viewdata extends MY_Controller {
+class View_data extends MY_Controller {
     public function index(){
 
     }
