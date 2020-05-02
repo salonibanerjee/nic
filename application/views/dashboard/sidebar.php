6f7a225ae9b783ed609d7633c121e74b54e205ce
Sayak <sayakdas1999@gmail.com>
2020-05-02 07:14:08 +0530

    Base scheme Load

--- application/views/dashboard/sidebar.php
+++ application/views/dashboard/sidebar.php
@@ -31,6 +31,7 @@
           <?php
             $data=$this->cache->get('Active_status')['user_type_id_fk'];
             $var=$this->cache->get('User_type'.$data)['user_privilege'];
+            $scheme_hier=$this->cache->get('scheme_hier_'.$this->session->userdata('dept'));
             $i=0;
             foreach($var as $x){
               if($x['view_sidebar']==1){
@@ -38,9 +39,15 @@
                 $var2= 'http://localhost/NIC/index.php/'.$x['link'];
                 echo "<li class='nav-item'>";
                 if($var1==$var2){
-                  echo "<a href='http://localhost/NIC/index.php/".$x['link']."' class='nav-link active'>";
+                  if($x['privilege_id_fk']==3 || $x['privilege_id_fk']== 4 ){
+                    echo "<a href='http://localhost/NIC/index.php/".$x['link']."/".$scheme_hier[0]['scheme_link']."' class='nav-link active'>";
+                  }else
+                    echo "<a href='http://localhost/NIC/index.php/".$x['link']."' class='nav-link active'>";
                 }else
-                  echo "<a href='http://localhost/NIC/index.php/".$x['link']."' class='nav-link'>";
+                  if($x['privilege_id_fk']==3 || $x['privilege_id_fk']== 4 ){
+                    echo "<a href='http://localhost/NIC/index.php/".$x['link']."/".$scheme_hier[0]['scheme_link']."' class='nav-link'>";
+                  }else
+                    echo "<a href='http://localhost/NIC/index.php/".$x['link']."' class='nav-link'>";
                 if($x['privilege_id_fk']==1||$x['privilege_id_fk']==3||$x['privilege_id_fk']==5){
                   echo "<i class='nav-icon fas fa-plus'></i>";
                 }else
