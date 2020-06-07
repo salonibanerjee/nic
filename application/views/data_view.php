4cd2cb8e64232c0ab5f9a9c822dcb59e3be96b46
Hriddhi Mondal <hriddhi1990@gmail.com>
2020-06-07 17:44:31 +0530

    Paging added in data_view

--- application/views/data_view.php
+++ application/views/data_view.php
@@ -40,7 +40,7 @@
             </div>
             <!-- /.card-header -->
             <div class="card-body">
-              <table id="example1" class="table table-bordered table-striped table-hover table-responsive equal-width">
+              <table id="example1" class="table table-bordered table-striped table-hover equal-width">
                 <thead class="bg-success">
                 <tr>
                   <?php $i=0; foreach($data as $row){
@@ -73,7 +73,7 @@
                                 echo "<td>".$row1."</td>";
                             }
                         }
-                        echo "<tr>";
+                        echo "</tr>";
                         $i++;
                     }
                 }
@@ -84,73 +84,71 @@
             <!-- /.card-body -->
           </div>
         </div>
-        
-      <!-- right column -->
-      <div class='col-md-1'>
-      
-      <input type="button" class='btn btn-primary' id="btnExport" value="Export" onclick="Export()" />
-      </div>
-      <!--/.col (right) -->
-      </div>
+        <!-- right column -->
+        <div class='col-md-1'>
+          <input type="button" class='btn btn-primary' id="btnExport" value="Export" onclick="Export()" />
+        </div>
+        <!--/.col (right) -->
+        </div>
       <!-- /.row -->
     </div><!-- /.container-fluid -->
   </section>
 </div>
 
 <div class="modal fade" id="modal-sm">
-      <form method="POST" id='draft' action="">
-        <div class="modal-dialog modal-default" >
-          <div class="modal-content">
-            <div class="modal-header bg-blue">
-              <h4 class="modal-title">Choose Month and Year Range</h4>
-              <button type="button" id="hi" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-
-              <div class="row">
-                <div class="col-md-6">
-                  <label>Start Month</label>
-                  <select id='modmonthst' name="modmonthst" class="form-control select2 select2-hidden-accessible">
-                  <option value='1' selected>January</option>
-                  <?php for($x=2;$x<=12;$x++){
-                                echo "<option value='".$x."'>".$month[$x]."</option>";
-                              }?>
-                  </select>
-                </div>
-                <div class="col-md-6">
-                  <label>Ending Month</label>
-                  <select id='modmonthend' name="modmonthend" class="form-control select2 select2-hidden-accessible">
-                  <option value='1' selected>January</option>
-                  <?php for($x=2;$x<=12;$x++){
-                                echo "<option value='".$x."'>".$month[$x]."</option>";
-                              }?>
-                  </select>
-                </div>
-                
-                <div class="col-md-12">      
-                  <label style="margin-top:10px;">Choose Year</label>
-                  <select id='modyear' name="modyear" class="form-control select2 select2-hidden-accessible">
-                  <?php $year=intval(date('Y')); 
-                  echo "<option value='".$year."' selected>".$year."</option>";
-                  for($x=$year-1;$x>=$year_range->financial_year_range;$x--){
-                                echo "<option value='".$x."'>".$x."</option>";
-                              }?>
-                  </select>
-                </div>
-                <div id="errors" style="color:red;"></div>
-              </div>
-            </div>
-            <div class="modal-footer justify-content-between">
-              <button type="submit" id="filter_sub" name="filter_sub" class="btn btn-outline-primary" style="display: block; margin-left: auto;  margin-right: auto;" form="draft" >Apply</button>
-            </div>
-          </div>
-          <!-- /.modal-content -->
+  <form method="POST" id='draft' action="">
+    <div class="modal-dialog modal-default" >
+      <div class="modal-content">
+        <div class="modal-header bg-blue">
+          <h4 class="modal-title">Choose Month and Year Range</h4>
+          <button type="button" id="hi" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div>
-      </form>
-        <!-- /.modal-dialog -->
+        <div class="modal-body">
+
+          <div class="row">
+            <div class="col-md-6">
+              <label>Start Month</label>
+              <select id='modmonthst' name="modmonthst" class="form-control select2 select2-hidden-accessible">
+              <option value='1' selected>January</option>
+              <?php for($x=2;$x<=12;$x++){
+                            echo "<option value='".$x."'>".$month[$x]."</option>";
+                          }?>
+              </select>
+            </div>
+            <div class="col-md-6">
+              <label>Ending Month</label>
+              <select id='modmonthend' name="modmonthend" class="form-control select2 select2-hidden-accessible">
+              <option value='1' selected>January</option>
+              <?php for($x=2;$x<=12;$x++){
+                            echo "<option value='".$x."'>".$month[$x]."</option>";
+                          }?>
+              </select>
+            </div>
+            
+            <div class="col-md-12">      
+              <label style="margin-top:10px;">Choose Year</label>
+              <select id='modyear' name="modyear" class="form-control select2 select2-hidden-accessible">
+              <?php $year=intval(date('Y')); 
+              echo "<option value='".$year."' selected>".$year."</option>";
+              for($x=$year-1;$x>=$year_range->financial_year_range;$x--){
+                            echo "<option value='".$x."'>".$x."</option>";
+                          }?>
+              </select>
+            </div>
+            <div id="errors" style="color:red;"></div>
+          </div>
+        </div>
+        <div class="modal-footer justify-content-between">
+          <button type="submit" id="filter_sub" name="filter_sub" class="btn btn-outline-primary" style="display: block; margin-left: auto;  margin-right: auto;" form="draft" >Apply</button>
+        </div>
       </div>
+      <!-- /.modal-content -->
+    </div>
+  </form>
+  <!-- /.modal-dialog -->
+</div>
 
 <!-- DataTables -->
 <script src="http://localhost/NIC/css/plugins/datatables/jquery.dataTables.js"></script>
@@ -166,26 +164,13 @@
       "searching": false,
       "ordering": true,
       "info": true,
-      "autoWidth": false,
+      "autoWidth": true,
     });
   });
 </script>
 
 <script src="http://localhost/NIC/css/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-<!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- DataTables -->
-<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
-<!-- page script -->
+
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.22/pdfmake.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
 <script type="text/javascript">
