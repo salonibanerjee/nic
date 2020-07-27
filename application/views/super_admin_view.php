0cd31d56109b5737a6b01a97311e47cd6366f5a0
ayertdatta <ganguly.riddhinath@gmail.com>
2020-07-28 02:54:42 +0530

    sql recent and funds from super admin deleted

--- application/views/super_admin_view.php
+++ application/views/super_admin_view.php
@@ -147,20 +147,6 @@
         </div>
 <!--------------------------------------- -->
         <div class="row">
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-              <div class="inner">
-                <h3>₹</h3>
-
-                <p>Funds Details</p>
-              </div>
-              <div class="icon">
-                <i>₹</i>
-              </div>
-              <a href="<?php echo base_url(); ?>index.php/Fund" class="small-box-footer">Update Funds<i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
 
           <div class="col-lg-3 col-6">
             <!-- small box -->
