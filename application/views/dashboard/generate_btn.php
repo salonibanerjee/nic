91a201e2cf7f7858c904404c8c2fa138a581d058
Sayak <sayakdas1999@gmail.com>
2020-08-06 21:29:45 +0530

    Fix

--- application/views/dashboard/generate_btn.php
+++ application/views/dashboard/generate_btn.php
@@ -8,16 +8,16 @@
     <div class="row text-center" style="padding-top: 5px">
         <div class="col-md-6">
         
-            <a href="https://localhost/NIC/index.php/Report/pdfreport"><button type="button" class="btn btn-block bg-danger" style="margin-bottom:3px;"><i class="fa fa-file-pdf float-left" style="font-size:30px;"></i>PDF Format</button></a>
+            <a href="http://localhost/NIC/index.php/Report/pdfreport"><button type="button" class="btn btn-block bg-danger" style="margin-bottom:3px;"><i class="fa fa-file-pdf float-left" style="font-size:30px;"></i>PDF Format</button></a>
         </div>
         <div class="col-md-6">
-            <a href="https://localhost/NIC/index.php/Report/excelreport"><button type="button" class="btn btn-block bg-success"><i class="fa fa-file-excel float-left" style="font-size:30px;",name="excel" id="excel"></i>Excel Format</button></a>
+            <a href="http://localhost/NIC/index.php/Report/excelreport"><button type="button" class="btn btn-block bg-success"><i class="fa fa-file-excel float-left" style="font-size:30px;",name="excel" id="excel"></i>Excel Format</button></a>
         </div>
     </div>
 </div>
 <script type="text/javascript">
     function generate()
     {
-        window.location.href="https://localhost/NIC/index.php/Report/pdfreport";
+        window.location.href="http://localhost/NIC/index.php/Report/pdfreport";
     }
 </script>
