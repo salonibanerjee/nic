261dc00d409af449beaa262931d26046ff1c5860
Sayak <sayakdas1999@gmail.com>
2020-08-24 13:26:41 +0530

    Fix

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * PDF
+ *
+ *
+ * @package		CodeIgniter
+ * @category	Library
+ * @author		Sachin Kumar Roy
+*/
 if(!defined('BASEPATH')) exit('No direct script access allowed');
 
 require_once dirname(__FILE__) . '/tcpdf/tcpdf.php';
